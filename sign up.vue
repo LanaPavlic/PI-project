2f2d453bddcd4306aaca06a894ccf08aed14c7c6
moira56 <mcekada1@student.unipu.hr>
2024-10-22 09:26:57 +0200

    VUE
    Prilagodba koda zab rad s VUEjs

--- sign up.vue
+++ sign up.vue
@@ -1,3 +1,36 @@
+<template>
+    <div class="form_area">
+        <h4 class="title">SIGN UP</h4>
+        <form action="">
+            <div class="form_group">
+                <label for="name" class="sub_title">Name</label>
+                <input type="text" class="form_style" id="name" placeholder="Enter your full name">
+            </div>
+            <div class="form_group">
+                <label for="email" class="sub_title">Email</label>
+                <input type="email" class="form_style" id="email" placeholder="Enter your email" />
+            </div>
+            <div class="form_group">
+                <label for="password" class="sub_title">Password</label>
+                <input type="password" class="form_style" id="password" placeholder="Enter your password" />
+            </div>
+            <div class="form_group">
+                <label for="repeat-password" class="sub_title">Repeat Password</label>
+                <input type="password" class="form_style" id="repeat-password" placeholder="Repeat your password" />
+            </div>
+            <div>
+                <button class="btn">SIGN UP!</button>
+                <p>Have an Account? <a href="" class="link">Login Here!</a></p>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+// kod js/vue
+</script>
+
+<style scoped>
 @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900');
 
 *{
@@ -85,4 +118,4 @@ p{
     font-weight: 800;
 }
 
-/* */
+</style>
